7f4f5d6e85440fa919372aba8001f5f532054682
Russell Dunphy <russell.dunphy@onthebeach.co.uk>
2012-11-23 16:10:19 +0000

    First commit

--- spec/facebook_likes/like_finder_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe LikeFinder do
-  it 'returns the number of likes we have on the facebook' do
-    LikeFinder.new.likes.should be_a Fixnum
-  end
-end
--- spec/spec_helper.rb
+++
@@ -1,6 +0,0 @@
-require 'rspec'
-require_relative '../facebook_likes'
-
-RSpec.configure do |c|
-  c.order = 'random'
-end
